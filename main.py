0f1b1102f23c5bdefcf116a1f2e469d81350041d
Thomassaiag <thomas.saiag@gmail.com>
2023-06-08 14:35:34 +0200

    display bug fix

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import random
 
 
 def displayGrid(table):
-    tableHeight = len(table)
+    tableHeight=len(table)
     for j in range(0, tableHeight):
         lines = ""
         content = ""
-        tableWith = len(table[j])
+        tableWith=len(table[j])
         for i in range(0, tableWith+1):
             if i == tableWith:
                 content += "|"
@@ -29,29 +29,23 @@ def createGrid(m, n):
             lineArray.append("O")
         globalArray.append(lineArray)
         # print(globalArray)
-    print(globalArray)
     return globalArray
 
 
 def fillGrigWithX(tableToFill, k):
     kArray = []
     gridHeight = len(tableToFill)
-    print(f"hauteur grille={gridHeight}")
     for _ in range(k):
         kArray.append("X")
-    for i in range(0, gridHeight):
-        print(f"i={i}")
-        gridWidth = len(tableToFill[i])
-        print(f"largeur grid:{gridWidth}")
-        for j in range(0, gridWidth):
-            print(f"j={j}")
-            randInt = random.randint(1, gridHeight)
-            print(f"randInt={randInt}")
-            if randInt == 1 and tableToFill[i][j] != "X" and len(kArray) != 0:
-                tableToFill[i][j] = "X"
-                kArray.pop()
-                print(f"longueur karray={len(kArray)}")
-
+    while len(kArray)>0:
+        for i in range(0, gridHeight):
+            gridWidth = len(tableToFill[i])
+            for j in range(0, gridWidth):
+                randInt = random.randint(1, gridHeight)
+                if randInt == 1 and tableToFill[i][j] != "X" and len(kArray)!=0:
+                    tableToFill[i][j] = "X"
+                    kArray.pop()
+                    print(f"longueur karray={len(kArray)}")
     print(tableToFill)
     return tableToFill
 
